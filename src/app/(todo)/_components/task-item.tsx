fb806166d6fc742eaeabf4322f4e2171d5e641a0
Alex <alffreitas@outlook.com>
2025-02-22 15:01:49 -0300

    refactor: component task item

---
+++ src/app/(todo)/_components/task-item.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { ConfirmDialog } from '@/components/confirm-dialog'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
+import { useTaskStore } from '@/store/task-store'
+import { Task } from '@/types/task'
+import { Trash2 } from 'lucide-react'
+
+type TaskItemProps = {
+  task: Task
+}
+
+export const TaskItem = ({ task }: TaskItemProps) => {
+  const { removeTask, toggleTask } = useTaskStore()
+
+  return (
+    <Card className="w-full p-2 pl-4 flex items-center gap-3">
+      <div className="flex items-center flex-1">
+        <Label className="flex items-center gap-2 cursor-pointer font-normal">
+          <Checkbox
+            className="flex-shrink-0 w-5 h-5 border-gray-300 data-[state=checked]:bg-blue-600 data-[state=checked]:border-blue-600"
+            onCheckedChange={() => toggleTask(task.id)}
+            checked={task.completed}
+          />
+          <span className={task.completed ? 'line-through opacity-60' : ''}>
+            {task.title}
+          </span>
+        </Label>
+      </div>
+      <ConfirmDialog
+        title="Remover tarefa?"
+        description="Ao confirmar, a tarefa será removida da lista"
+        onConfirm={() => removeTask(task.id)}
+      >
+        <Button variant="ghost" size="sm" className="flex-shrink-0">
+          <Trash2 className="text-destructive" />
+        </Button>
+      </ConfirmDialog>
+    </Card>
+  )
+}
